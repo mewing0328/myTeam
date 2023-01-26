94f1decc42ca6b0e13b3f24facd8543a54372f62
MEwing <masandraewing@gmail.com>
2023-01-26 11:38:40 -0500

    All tests run correctly

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,7 +1,42 @@
 const Employee = require('../lib/Employee');
 
+// name test
 test("Can utilize the constructor to create this name for this instance", () => {
-    const name = "MaSandra";
-    const employee = new Employee(name);
+    const name = 'MaSandra';
+    const employee = new Employee("MaSandra", 123, "masandra@123.com");
     expect(employee.name).toBe(name);
   });
+
+test('the getName() function is working', async () => {
+  const name = 'MaSandra';
+  const employee = new Employee("MaSandra", 123, "masandra@123.com");
+  expect(employee.getName()).toBe(name);
+});
+
+
+// ID test
+test("Can utilize the constructor to create this id for this instance", () => {
+  const id = 123;
+  const employee = new Employee("MaSandra", 123, "masandra@123.com");
+  expect(employee.id).toBe(id);
+});
+
+test('the getId() function is working', async () => {
+const id = 123;
+const employee = new Employee("MaSandra", 123, "masandra@123.com");
+expect(employee.getId()).toBe(id);
+});
+
+
+// email test
+test("Can utilize the constructor to create this email for this instance", () => {
+  const email = "masandra@123.com";
+  const employee = new Employee("MaSandra", 123, "masandra@123.com");
+  expect(employee.email).toBe(email);
+});
+
+test('the getEmail() function is working', async () => {
+const email = 'masandra@123.com';
+const employee = new Employee("MaSandra", 123, "masandra@123.com");
+expect(employee.getEmail()).toBe(email);
+});
--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -5,3 +5,20 @@ test("Can utilize the constructor to create this school for this instance", () =
     const intern = new Intern("MaSandra", 123, "masandra@123.com", addlInfo, "Intern");
     expect(intern.addlInfo).toBe(addlInfo);
   });
+
+  test('the getSchool() function is working', async () => {
+    const addlInfo = 'Iowa State';
+    const intern = new Intern("MaSandra", 123, "masandra@123.com", addlInfo, "Intern");
+    expect(intern.getSchool()).toBe(addlInfo);
+  });
+  
+  test('the engineer has "Engineer" in it', () => {
+    const intern = new Intern("MaSandra", 123, "masandra@123.com", 'Iowa State', "Intern");
+    expect(intern.role).toBe('Intern');
+  });
+  
+  test('the getRole() function is working', async () => {
+    const role = 'Intern';
+    const intern = new Intern("MaSandra", 123, "masandra@123.com", 'Iowa State', "Intern");
+    expect(intern.getRole()).toBe(role);
+  });
